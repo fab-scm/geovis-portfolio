8ff398faae82806d7d060324d9bf937e6083ee02
fab-scm <fabian.schmcr@gmail.com>
2025-03-18 18:04:34 +0100

    update portfolio

---
+++ pages/portfolio/projects/images.js
@@ -0,0 +1,14 @@
+const images = [
+  {
+    original: "/geovis-portfolio/portfolio/projects/rusle/dem_tile_N.png",
+    thumbnail: "/geovis-portfolio/portfolio/projects/rusle/dem_tile_N.png",
+  },
+  {
+    original: "/geovis-portfolio/portfolio/projects/rusle/dem_tile_S.png",
+    thumbnail: "/geovis-portfolio/portfolio/projects/rusle/dem_tile_S.png",
+  },
+  {
+    original: "/geovis-portfolio/portfolio/projects/rusle/dem_mosaic.png",
+    thumbnail: "/geovis-portfolio/portfolio/projects/rusle/dem_mosaic.png",
+  },
+]
